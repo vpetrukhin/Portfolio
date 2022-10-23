051a2de2693faef8778e791ebac1e8829f560c88
Vasiliy Petrukhin <vpetruin@inbox.ru>
2022-10-23 16:13:32 +0300

    Add button

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,26 @@
+import { ButtonHTMLAttributes } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './Button.module.scss';
+
+export enum ButtonTheme {
+    primary = 'primary',
+    secondary ='secondary',
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    className?: string;
+    theme?: ButtonTheme;
+}
+
+export const Button = (props: ButtonProps) => {
+    const { className, children, theme = ButtonTheme.primary, ...otherProps } = props;
+
+    return (
+        <button
+            className={classNames(cls.Button, {}, [className, cls[theme]])}
+            {...otherProps}
+        >
+            {children}
+        </button>
+    );
+};
