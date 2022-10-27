0ec24c18004456aed80f569ddb409c058c3ca24a
Vasiliy Petrukhin <vpetruin@inbox.ru>
2022-10-27 13:17:01 +0300

    add Footer

---
+++ src/sections/Footer/ui/Footer.tsx
@@ -0,0 +1,24 @@
+import { Sections } from 'shared/config/nav/items';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Text, TextColor, TextSize } from 'shared/ui/Text/Text';
+import { SocialLinks } from 'widgets/SocialLinks/SocialLinks';
+import cls from './Footer.module.scss';
+
+interface FooterProps {
+    className?: string;
+}
+
+export const Footer = (props: FooterProps) => {
+    const { className } = props;
+
+    return (
+        <section id={`#${Sections.CONTACS}`} className={classNames(cls.Footer, {}, [className])}>
+            <SocialLinks />
+            <ul className={cls.contacs}>
+                <li className={cls.contact}>
+                    <Text text="Почта: vpetruin16@gmail.com" color={TextColor.PRIMARY} size={TextSize.M} />
+                </li>
+            </ul>
+        </section>
+    );
+};
